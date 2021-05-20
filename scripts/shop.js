1da6b49489caeb02378f280f3a9d259724dca874
Jacob Khaliqi <jkhaliqi@ucsd.edu>
2021-05-20 14:25:48 -0700

    Add shopping screen items!

---
+++ scripts/shop.js
@@ -0,0 +1,13 @@
+/*
+*Code for Home Screen that will send user to actual shop
+*/
+function screen(){
+    document.getElementById("homeScreen").style.display="none";
+    document.getElementById("shopScreen").style.visibility="visible";
+    document.body.style.backgroundColor="white";
+};
+
+
+
+
+
